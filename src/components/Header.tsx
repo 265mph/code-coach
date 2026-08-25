7e827cbc4b10a230985ffcb01bda57a90b02f47f
Aina Temiloluwa Samuel <samuelaina18@gmail.com>
2026-08-25 20:27:15 +0100

    code coach logo change

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+// import CCwordmark from "./../assets/images/codecoach-wordmark.svg";
+import Logo from "./Logo";
+
+const Header = () => {
+  return (
+    <div className="w-full flex item-center p-5">
+      <Logo />
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Logo.tsx
@@ -0,0 +1,13 @@
+const Logo = () => {
+  return (
+    <>
+      <p className="font-inter font-semibold text-[1.5rem] text-green-600">
+        {`< Code`}
+        <span className="text-mist-900">Coach</span>
+        {` />`}
+      </p>
+    </>
+  );
+};
+
+export default Logo;
