40500351f042b505f48cb2f5b43b5e9fc79be4c8
Aina Temiloluwa Samuel <samuelaina18@gmail.com>
2026-08-29 22:46:33 +0100

    difficulty select

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Canvas from "./components/Canvas"
 import Header from "./components/Header"
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Canvas />
     </div>
   )
 }
